d0c422236bed7216d913268167f74355e6a22aa2
Venus Lumanglas <vlumanglas30@gmail.com>
2024-01-11 14:20:11 +0800

    Add posts helper spec

---
+++ spec/helpers/posts_helper_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+# Specs in this file have access to a helper object that includes
+# the PostsHelper. For example:
+#
+# describe PostsHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+RSpec.describe PostsHelper do
+  let(:user_john) do
+    User.create!(email: "jd@gmail.com", password: "pass123", first_name: "John", last_name: "Doe")
+  end
+
+  let(:user_jane) do
+    User.create!(email: "jdoe@gmail.com", password: "pass123", first_name: "Jane", last_name: "Doe")
+  end
+
+  let(:post) { Post.create!({ user_id: user_john.id, title: "MyPostTitle", body: "MyPostBody" }) }
+
+  describe "#post_author?" do
+    it "returns true if post author is equal to current user" do
+      sign_in(user_john)
+      expect(helper.post_author?(post)).to be true
+    end
+
+    it "returns false if post author is not equal to current user" do
+      sign_in(user_jane)
+      expect(helper.post_author?(post)).to be false
+    end
+  end
+end
