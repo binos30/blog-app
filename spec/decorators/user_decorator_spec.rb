27c7f2e8fa4d498463b391921d06623a06c2e85c
Venus Lumanglas <vlumanglas30@gmail.com>
2024-02-05 13:48:01 +0800

    Decorate user

---
+++ spec/decorators/user_decorator_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe UserDecorator do
+  let(:user) do
+    User.create!(
+      email: "jd@gmail.com",
+      password: "pass123",
+      first_name: "John",
+      last_name: "Doe"
+    ).decorate
+  end
+
+  it "returns the full name" do
+    expect(user.full_name).to eq("John Doe")
+  end
+
+  it "returns the initials" do
+    expect(user.initials).to eq("JD")
+  end
+end
