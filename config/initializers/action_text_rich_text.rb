38684488d3f7bfa7ab8bf53f272d5f8e75c816ee
Venus Lumanglas <vlumanglas30@gmail.com>
2024-03-03 18:09:17 +0800

    Temporarily remove `action_text_rich_text.rb`

--- config/initializers/action_text_rich_text.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-ActiveSupport.on_load :action_text_rich_text do
-  before_save { self.plain_text_body = body.to_plain_text }
-
-  scope :with_body_containing, ->(query) { where <<~SQL.squish, query }
-    to_tsvector('english', plain_text_body) @@ websearch_to_tsquery(?)
-  SQL
-end
